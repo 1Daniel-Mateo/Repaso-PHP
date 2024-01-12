d2988dbb12f223335e7e396057582f99f987b819
Daniel Mateo Suarez Alvarez <dsuarez@adgasd32.int>
2024-01-12 16:56:33 -0500

    FALTA ACTUALIZAR

--- controller/crudControlador.php
+++ controller/crudControlador.php
@@ -11,7 +11,7 @@ $modelo = $_POST['modelo'];
 $descripcion2 = $_POST['descripcion2'];
 
 if (isset($_POST)) {   
-    if(empty($serial) || empty($placa) || empty($descripcion1) || empty($marca) || empty($modelo) || empty($descripcion2)){
+    if(empty($serial) || empty($placa) || empty($descripcion1) || empty($marca) || empty($modelo)){
         header('Location: ../index.php?m=1&m=0');
         echo "campos vacios";
     }else {
@@ -27,7 +27,9 @@ if (isset($_POST)) {
 
 
 //Controlador de actualizacion
-$idU = $_POST['idActivo'];
+if (isset($_GET['idActivo'])) {
+//Variables que van a ser tomadas para el parametro
+$idU = $_GET['idActivo'];
 $Userial = $_POST['Userial'];
 $Uplaca = $_POST['Uplaca'];
 $Udescripcion1 = $_POST['Udescripcion1'];
@@ -35,10 +37,14 @@ $Umarca = $_POST['Umarca'];
 $Umodelo = $_POST['Umodelo'];
 $Udescripcion2 = $_POST['Udescripcion2'];
 
+//comprobador en consola para evaluar si detecta al id
 error_log("ID del activo: " . $idU);
 
+//condicion para evaluar si se actualizan o no los datos
 if (isset($_POST)) {
+    //variable para la conexion con la funcion del modelo
         $actualizar = actualizar($idU,$Userial,$Uplaca,$Udescripcion1,$Umarca,$Umodelo,$Udescripcion2);
+        //condicion de evualacion si actualizo los datos o no
         if ($actualizar) {
             header('Location: ../index.php?m=1');
             echo "Actualizado";
@@ -46,7 +52,7 @@ if (isset($_POST)) {
             header('Location: ../views/actualizar.php?m=0');
         }
 }
-
+}
 
 //controlador eliminar
 if (isset($_GET['idActivo'])) {
